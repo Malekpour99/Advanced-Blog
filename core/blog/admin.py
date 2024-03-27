e742914586761de61e45e5b699d4480ed222b8fe
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-03-27 12:08:34 +0330

    blog admin - Post & Category registration
    Registered Post and Category model to the admin panel

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from blog.models import Post, Category
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "author",
+        "title",
+        "published",
+        "category",
+        "created_date",
+        "published_date",
+    )
+    list_filter = ("author", "published")
+    empty_value_display = "-empty-"
+    date_hierarchy = "created_date"
+    search_fields = ["title", "content"]
+
+
+admin.site.register(Category)
