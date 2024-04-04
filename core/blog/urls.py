fc56fc12b710a45e622ddaba0d6177a8d5bd29d7
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-04 12:10:45 +0330

    blog URLs - API URL
    Included blog app API URLs in the blog URL paths

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
+app_name = "blog"
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("api/v1/", include("blog.api.v1.urls")),
 ]
