fb2910247bae256c86026b3cbeea2418432ef4ec
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-06 05:35:45 +0330

    blog API views - post_detail status code
    Used REST status module for more readability in the returning status for objects

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework import status
 
 
 @api_view()
@@ -16,4 +17,7 @@ def post_detail(request, id):
         serializer = PostSerializer(post)
         return Response(serializer.data)
     except Post.DoesNotExist:
-        return Response({"detail": "Post does not exist"}, status=404)
+        # return Response({"detail": "Post does not exist"}, status=404)
+        return Response(
+            {"detail": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND
+        )
