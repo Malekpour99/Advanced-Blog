ceab56f071cb97b322e059da6ab62b15ad4e68cd
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-06 05:31:11 +0330

    blog API - post_detail
    Created post_detail for reading and returning posts from Post model using serializers and REST framework Response

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,7 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from blog.models import Post
+from .serializers import PostSerializer
 
 
 @api_view()
 def api_rest_view(request):
     return Response("ok")
+
+
+@api_view()
+def post_detail(request, id):
+    try:
+        post = Post.objects.get(pk=id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    except Post.DoesNotExist:
+        return Response({"detail": "Post does not exist"}, status=404)
