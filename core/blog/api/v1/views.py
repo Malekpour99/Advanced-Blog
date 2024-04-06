1c02836c1a7ceef07a74f7a1f6aa294195b14db7
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-06 05:43:07 +0330

    blog API views - post_detail get_object_or_404
    Summarized post_detail function using the get_object_or_404 method for finding our desired post object

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from blog.models import Post
 from .serializers import PostSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 @api_view()
@@ -10,14 +11,21 @@ def api_rest_view(request):
     return Response("ok")
 
 
+# @api_view()
+# def post_detail(request, id):
+#     try:
+#         post = Post.objects.get(pk=id)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#     except Post.DoesNotExist:
+#         # return Response({"detail": "Post does not exist"}, status=404)
+#         return Response(
+#             {"detail": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND
+#         )
+        
+# You can summarize above code like below:
 @api_view()
 def post_detail(request, id):
-    try:
-        post = Post.objects.get(pk=id)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    except Post.DoesNotExist:
-        # return Response({"detail": "Post does not exist"}, status=404)
-        return Response(
-            {"detail": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND
-        )
+    post = get_object_or_404(Post, pk=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
