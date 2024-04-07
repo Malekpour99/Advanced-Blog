f90f8002ba91265ef076ef373dd5cdf1bc901942
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-07 04:45:38 +0330

    blog API serializers - Post ModelSerializer
    Converted PostSerializer to a Model Serializer for better field management and getting model fields

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
+from blog.models import Post
 
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        # don't use __all__ for fields (best practice)
+        fields = [
+            "id",
+            "title",
+            "content",
+            "published",
+            "published_date",
+            "created_date",
+        ]
