14d6714c592a230657d7d97e935f8781b8ef075d
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-09 04:55:42 +0330

    blog API URLs - post-list
    Changed base path from 'posts' to 'post' and dedicated the new post list CBV to this URL path

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "api-v1"
 
 urlpatterns = [
-    path("posts", views.post_list, name="post-list"),
+    # path("post/", views.post_list, name="post-list"),
+    path("post/", views.PostList.as_view(), name="post-list"),
     path("post/<int:id>/", views.post_detail, name="post-detail"),
 ]
