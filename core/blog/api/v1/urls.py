10b7f794c48d7399fdcbfddb3974396a2dcfeb5d
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-10 20:23:07 +0330

    blog API URLs - PostViewSet URLs
    Replaced previous URL paths by new paths which are connected to the PostViewSet CBV

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -6,7 +6,24 @@ app_name = "api-v1"
 urlpatterns = [
     # path("post/", views.post_list, name="post-list"),
     # path("post/<int:id>/", views.post_detail, name="post-detail"),
-    path("post/", views.PostList.as_view(), name="post-list"),
+    # path("post/", views.PostList.as_view(), name="post-list"),
     # path("post/<int:id>/", views.PostSingle.as_view(), name="post-detail"),
-    path("post/<int:pk>/", views.PostSingle.as_view(), name="post-detail"),
+    # path("post/<int:pk>/", views.PostSingle.as_view(), name="post-detail"),
+    path(
+        "post/",
+        views.PostViewSet.as_view({"get": "list", "post": "create"}),
+        name="post-list",
+    ),
+    path(
+        "post/<int:pk>/",
+        views.PostViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="post-detail",
+    ),
 ]
