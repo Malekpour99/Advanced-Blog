871bfd9475fcd6fa58809a9d1fb73043424785ec
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-12 13:36:14 +0330

    blog API serializers - Post serializer relative_url
    Added a relative_url field to the Post model serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -9,6 +9,7 @@ from blog.models import Post, Category
 
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
+    relative_url = serializers.URLField(source="get_relative_api_url")
 
     class Meta:
         model = Post
@@ -19,10 +20,14 @@ class PostSerializer(serializers.ModelSerializer):
             "title",
             "content",
             "snippet",
+            "relative_url",
             "published",
             "published_date",
             "created_date",
         ]
+
+    # If your methods and fields depend on the user request define them here in the serializer
+    # otherwise you can define them directly in your model class
 
 
 class CategorySerializer(serializers.ModelSerializer):
