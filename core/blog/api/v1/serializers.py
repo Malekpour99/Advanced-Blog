f53b34cb2a07eae02ebcfe5b1cb7f4cb93c8690c
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-13 12:07:32 +0330

    blog API serializers - Post serializer create
    Overrode the create method of the post serializer to assign the author by checking the authenticated user and also added author list to the read-only fields

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from blog.models import Post, Category
+from accounts.models import Profile
 
 
 # class PostSerializer(serializers.Serializer):
@@ -39,13 +40,14 @@ class PostSerializer(serializers.ModelSerializer):
             "published_date",
             "created_date",
         ]
+        read_only_fields = ["author"]
 
     # If your methods and fields depend on the user request define them here in the serializer
     # otherwise you can define them directly in your model class
     def get_absolute_url(self, obj):
         request = self.context.get("request")
         return request.build_absolute_uri(obj.pk)
-    
+
     def to_representation(self, instance):
         request = self.context.get("request")
         rep = super().to_representation(instance)
@@ -57,3 +59,9 @@ class PostSerializer(serializers.ModelSerializer):
             rep.pop("content", None)
         rep["category"] = CategorySerializer(instance.category).data
         return rep
+
+    def create(self, validated_data):
+        validated_data["author"] = Profile.objects.get(
+            user__id=self.context.get("request").user.id
+        )
+        return super().create(validated_data)
