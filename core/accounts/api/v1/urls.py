3b7558d4d384e5d7d7879a3b9e436a340f3432d9
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-23 19:06:46 +0330

    accounts api URLs - registration URL
    Created URL path for registration of new users using the UserRegistration CBV

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "api-v1"
+
 urlpatterns = [
-    
+    path("registration/", views.UserRegistration.as_view(), name="registration"),
 ]
