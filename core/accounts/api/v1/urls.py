3e6fad81bf09d05e3fd7d9e0f3232d7cdd10a8d9
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-25 10:25:41 +0330

    accounts API URLs - login token URL custom view
    Replaced the rest framework built-in view with our custom obtain auth token class

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = "api-v1"
 
 urlpatterns = [
     path("registration/", views.UserRegistration.as_view(), name="registration"),
-    path("token/login/", ObtainAuthToken.as_view(), name="token-login"),
+    path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
 ]
