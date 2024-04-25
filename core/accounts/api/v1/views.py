1b0361209a7d0505c432003777878528df3a351e
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-25 10:40:29 +0330

    accounts API views - custom discard auth token
    Created custom discard auth token class which inherits from APIVeiw and deletes the existing auth token of the logged in user

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 
 class UserRegistration(GenericAPIView):
@@ -35,3 +37,11 @@ class CustomObtainAuthToken(ObtainAuthToken):
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
         return Response({"token": token.key, "user_id": user.pk, "email": user.email})
+
+
+class CustomDiscardAuthToken(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
