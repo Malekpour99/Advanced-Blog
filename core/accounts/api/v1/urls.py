8f39d49ee2f7b99911aa20dcdd8d1395c5a6c410
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-25 10:01:34 +0330

    accounts api URLs - auth token URL
    Created a new path for obtaining a unique auth token for users based on their username and password by built-in rest framework class

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = "api-v1"
 
 urlpatterns = [
     path("registration/", views.UserRegistration.as_view(), name="registration"),
+    path("token/login/", ObtainAuthToken.as_view(), name="token-login"),
 ]
