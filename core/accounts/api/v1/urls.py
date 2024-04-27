36d2f205f663d305fef1965257491a259851094f
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-27 05:42:14 +0330

    accounts API URLs - JWT URLs
    Created URL paths for the JWT authentication process which includes create, refresh and verify actions for JWT

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 app_name = "api-v1"
 
@@ -7,4 +12,7 @@ urlpatterns = [
     path("registration/", views.UserRegistration.as_view(), name="registration"),
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
+    path("jwt/create/", TokenObtainPairView.as_view(), name="jwt-create"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
+    path("jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
 ]
