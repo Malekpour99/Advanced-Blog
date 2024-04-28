e60a5b7f3f49984e10557115a0fc0e9c924cd1a2
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-28 05:33:22 +0330

    accounts API URLs - JWT create URL path view
    Replaced the JWT create URL path view with our custom view

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 app_name = "api-v1"
 
@@ -12,7 +8,7 @@ urlpatterns = [
     path("registration/", views.UserRegistration.as_view(), name="registration"),
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
-    path("jwt/create/", TokenObtainPairView.as_view(), name="jwt-create"),
+    path("jwt/create/", views.CustomTokenObtainPairView.as_view(), name="jwt-create"),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
 ]
