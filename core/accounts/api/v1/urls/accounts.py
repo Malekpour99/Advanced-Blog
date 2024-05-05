d02b5d5ce71a9e87af1eb38250dfb362210e3655
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-05 19:19:04 +0330

    accounts API accounts URLs - activation confirm
    Created a URL path for activating users based on their JWT token

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path("registration/", views.UserRegistration.as_view(), name="registration"),
+    path("activation/confirm/<str:token>", views.ActivationAPIView.as_view(), name="activation"),
     path("change-password/", views.ChangePasswordAPIView.as_view(), name="change-password"),
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
