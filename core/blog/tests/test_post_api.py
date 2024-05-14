b2ec7f1cab48bc72a1df8c89b07a357f6952cc0a
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-14 18:50:27 +0330

    blog tests - test post API creating a blog post
    Created a new test for creating a valid blog post by an authenticated user

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -38,3 +38,18 @@ class TestPostAPI:
         }
         response = api_client.post(url, data, format="json")
         assert response.status_code == 401
+
+    def test_create_post_response_201_status(self, api_client, common_user):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title": "test title",
+            "content": "test content",
+            "published": True,
+            "published_date": datetime.now(),
+        }
+        # authentication with an invalid user
+        # self.client.force_authenticate(user={})
+        # both 'force_authenticate' and 'force_login' can be used for authentication
+        api_client.force_login(user=common_user)
+        response = api_client.post(url, data, format="json")
+        assert response.status_code == 201
