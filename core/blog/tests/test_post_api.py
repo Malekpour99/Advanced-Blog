9785e87cea245d4a8adc52e11336cede0e9b7f6e
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-14 05:35:27 +0330

    blog tests - test post API creating post
    Created a new test to test the process of creation of a blog post without being authenticated

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,13 +1,26 @@
 from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
+from datetime import datetime
 
 
 # Giving access to database
 @pytest.mark.django_db
 class TestPostAPI:
+    client = APIClient()
+
     def test_get_post_response_200_status(self):
         url = reverse("blog:api-v1:post-list")
-        client = APIClient()
-        response = client.get(url)
+        response = self.client.get(url)
         assert response.status_code == 200
+
+    def test_create_post_response_401_status(self):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title": "test title",
+            "content": "test content",
+            "published": True,
+            "published_date": datetime.now(),
+        }
+        response = self.client.post(url, data, format="json")
+        assert response.status_code == 401
