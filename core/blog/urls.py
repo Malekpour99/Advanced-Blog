047cc14c22a25de3fc3df776eb7c9a46a9d0648d
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-24 18:40:35 +0330

    blog URLs - mock server test URL
    Added a URL for testing the postman mock server which is mapped to its relevant view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from . import views
 
 app_name = "blog"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("mock-test/", views.postman_mock_server_test, name="postman-test"),
     path("api/v1/", include("blog.api.v1.urls")),
 ]
