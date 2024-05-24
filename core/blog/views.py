86cde094d5fe20be2e53b10ec42914563ae96dc2
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-24 18:39:43 +0330

    blog views - postman mock server test
    Created a FBV for testing a custom postman mock server and returning a JSON response

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import requests
 
-# Create your views here.
+
+def postman_mock_server_test(request):
+    response = requests.get(
+        "https://0e0ecd6a-4a53-4847-86ad-60cdb6dccedd.mock.pstmn.io/test/delay/5"
+    )
+    return JsonResponse(response.json())
