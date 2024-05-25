0e864fd5cf56ad9c97edb76a043af1e1a679f49a
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-25 05:21:52 +0330

    blog views - caching postman mock server response
    Applied basic caching for the postman mock server response in its relevant view

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,9 +1,13 @@
 from django.http import JsonResponse
+from django.core.cache import cache
 import requests
 
 
 def postman_mock_server_test(request):
-    response = requests.get(
-        "https://0e0ecd6a-4a53-4847-86ad-60cdb6dccedd.mock.pstmn.io/test/delay/5"
-    )
-    return JsonResponse(response.json())
+    if cache.get("test_delay_api") is None:
+        response = requests.get(
+            "https://0e0ecd6a-4a53-4847-86ad-60cdb6dccedd.mock.pstmn.io/test/delay/5"
+        )
+        # You can set the timeout locally as well (default: 5 minutes globally)
+        cache.set("test_delay_api", response.json(), 120)
+    return JsonResponse(cache.get("test_delay_api"))
